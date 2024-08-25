9c4d292c7800e06bdda87078c1cebddd796d08b2
wonsuk71 <lkwnephew@naver.com>
2024-08-25 09:54:06 +0900

    테스트입니다

---
+++ main.ts
@@ -0,0 +1,27 @@
+console.log('hello');
+
+
+let size:number = 100;
+size = 105;
+
+
+
+let product = {
+  id: 'c001',
+  name: '라이트 윈드 브레이커',
+  price: 129000,
+  sizes: ['M', 'L', 'XL'],
+};
+
+// ...
+
+const newProduct = {
+  id: 'c002',
+  name: '다크 윈드 브레이커',
+  price: 139000,
+  sizes: [90, 95, 100, 105, 100],
+};
+
+// ...
+
+product = newProduct;
